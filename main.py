e07f09ead6049bd5417cba4dce03d3a11e49bb5a
Arturo OA <arturoa91@gmail.com>
2022-12-28 12:53:38 -0300

    Add dungeon generation in procgen module

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import tcod
 
 from engine import Engine
 from entity import Entity
-from game_map import GameMap
 from input_handlers import EventHandler
+from procgen import generate_dungeon
 
 
 def main() -> None:
@@ -31,7 +31,7 @@ def main() -> None:
 
     entities = {npc, player}
 
-    game_map = GameMap(map_width, map_height)
+    game_map = generate_dungeon(map_width, map_height)
 
     engine = Engine(entities=entities,
                     event_handler=event_handler,
